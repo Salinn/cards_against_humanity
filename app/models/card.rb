f6ef35cf908407bdaea295f46e63ddf2fe160fae
salinn <pmd6624@rit.edu>
2015-08-28 15:24:54 -0400

    initial

---
+++ app/models/card.rb
@@ -0,0 +1,6 @@
+class Card < ActiveRecord::Base
+  COLORS = ['White', 'Black']
+end
+
+#490 white cards
+#90 black
---
+++ app/views/cards/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @card, :id, :color, :content, :created_at, :updated_at
